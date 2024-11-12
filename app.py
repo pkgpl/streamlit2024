1bdb1b09a6723857ebf537c1d440f54c5cc68355
Wansoo Ha <pkgpl@users.noreply.github.com>
2024-11-12 15:15:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 api_key = st.text_input("OpenAI API Key", type='password')
+client = OpenAI(api_key=api_key)
 
 query = st.text_area("Prompt")
 answer = ''
