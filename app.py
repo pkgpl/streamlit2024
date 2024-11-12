254ecba700271fe693bed8adf3378a847af1c69b
Wansoo Ha <pkgpl@users.noreply.github.com>
2024-11-12 15:13:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 import streamlit as st
-st.write("Hello World")
+from openai import OpenAI
+
+api_key = st.text_input("OpenAI API Key", type='password')
+
+prompt = st.text_area("Prompt")
+answer = ''
+
+if st.button("Generate"):
+    messages = [
+        {"role": "user", "content": query}
+    ]
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages
+    )
+    answer = response.choices[0].message.content
+
+st.text(answer)
