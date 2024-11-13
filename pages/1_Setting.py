59cb3b131dbe9e3646a14d697b78d438937b35b3
Wansoo Ha <pkgpl@users.noreply.github.com>
2024-11-13 06:04:40 +0000

    use correct api key

--- pages/1_Setting.py
+++ pages/1_Setting.py
@@ -4,10 +4,11 @@ from openai import OpenAI
 api_key = st.text_input("OpenAI API Key", 
                         value=st.session_state.get('api_key',''),
                         type='password')
-st.session_state['api_key'] = api_key
 
-if 'openai_client' in st.session_state:
-    client = st.session_state['openai_client']
-else:
-    client = OpenAI(api_key=api_key)
-    st.session_state['openai_client'] = client
+if api_key:
+    st.session_state['api_key'] = api_key
+    if 'openai_client' in st.session_state:
+        client = st.session_state['openai_client']
+    else:
+        client = OpenAI(api_key=api_key)
+        st.session_state['openai_client'] = client
