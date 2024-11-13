4e06263c57a1dcaff44b29a4ad2abd252353ab22
Wansoo Ha <pkgpl@users.noreply.github.com>
2024-11-13 05:50:22 +0000

    save client in session_state

--- pages/1_Setting.py
+++ pages/1_Setting.py
@@ -3,4 +3,8 @@ from openai import OpenAI
 
 api_key = st.text_input("OpenAI API Key", key='api_key', type='password')
 
-client = st.session_state.get('openai_client', OpenAI(api_key=api_key))
+if 'openai_client' in st.session_state:
+    client = st.session_state['openai_client']
+else:
+    client = OpenAI(api_key=api_key)
+    st.session_state['openai_client'] = client
